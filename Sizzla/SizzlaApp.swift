5927b58e81fb0abf9bbeddbc9bf95697178763a8
danlaratta <dan.laratta.jr@gmail.com>
2024-10-24 13:36:00 -0400

    Initial Commit

---
+++ Sizzla/SizzlaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SizzlaApp.swift
+//  Sizzla
+//
+//  Created by Daniel Laratta on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SizzlaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
