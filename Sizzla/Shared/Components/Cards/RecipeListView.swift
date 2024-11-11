1214897e0a18424abb99a5c4f508d3297e35d930
danlaratta <dan.laratta.jr@gmail.com>
2024-11-11 17:45:41 -0500

    tried getting save/heart button to work for 2 days and still does not work right, fuck it for now.

---
+++ Sizzla/Shared/Components/Cards/RecipeListView.swift
@@ -0,0 +1,53 @@
+//
+//  RecipeListView.swift
+//  Sizzla
+//
+//  Created by Daniel Laratta on 11/3/24.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    @Binding var isGridView: Bool
+    let columns = Array(repeating: GridItem(.flexible()), count: 2)
+    
+    let recipeData: [Recipe] = [
+        /*
+         Recipe(image: "burger", name: "Cheese Burger", category: "Dinner", timeCook: "20 mins", rating: "4.76"),
+         Recipe(image: "burger", name: "Cheese Burger", category: "Dinner", timeCook: "20 mins", rating: "4.76"),
+         Recipe(image: "burger", name: "Cheese Burger", category: "Dinner", timeCook: "20 mins", rating: "4.76"),
+         Recipe(image: "burger", name: "Cheese Burger", category: "Dinner", timeCook: "20 mins", rating: "4.76"),
+         Recipe(image: "burger", name: "Cheese Burger", category: "Dinner", timeCook: "20 mins", rating: "4.76"),
+         Recipe(image: "burger", name: "Cheese Burger", category: "Dinner", timeCook: "20 mins", rating: "4.76"),
+         */
+        Recipe(image: "burger", name: "Cheese Burger", timeCook: "20 mins", rating: "4.76"),
+        Recipe(image: "burger", name: "Cheese Burger", timeCook: "20 mins", rating: "4.76"),
+        Recipe(image: "burger", name: "Cheese Burger", timeCook: "20 mins", rating: "4.76"),
+        Recipe(image: "burger", name: "Cheese Burger", timeCook: "20 mins", rating: "4.76"),
+        Recipe(image: "burger", name: "Cheese Burger", timeCook: "20 mins", rating: "4.76"),
+        Recipe(image: "burger", name: "Cheese Burger", timeCook: "20 mins", rating: "4.76"),
+        Recipe(image: "burger", name: "Cheese Burger", timeCook: "20 mins", rating: "4.76"),
+    ]
+    
+    var body: some View {
+        ScrollView {
+            if !isGridView {
+                ForEach(recipeData, id: \.self) { recipe in
+                    RecipeCard(image: recipe.image, name: recipe.name, timeCook: recipe.timeCook, rating: recipe.rating)
+                }
+            }
+            else {
+                LazyVGrid(columns: columns) {
+                    ForEach(recipeData, id: \.self) { recipe in
+                        GridRecipeCard(image: recipe.image, name: recipe.name, timeCook: recipe.timeCook, rating: recipe.rating)
+                    }
+                }
+            }
+        }
+        .scrollIndicators(.hidden)
+    }
+}
+
+#Preview {
+    RecipeListView(isGridView: .constant(false))
+}
